18da94e5e429dabc4fbaf946bdddd8334886f04d
Jeonghyun Ryu <jhforstudy@gmail.com>
2023-11-21 11:45:13 +0900

    클래스 변수 관련 설명 추가

--- scripts/brain.py
+++ scripts/brain.py
@@ -16,6 +16,14 @@ param = Param()
 class Brain():
     def __init__(self, db=Database, map_number=int):
         self.db = db
+        '''
+        이 곳은 클래스가 처음 선언될 때 최초로 실행되는 함수입니다.
+
+        여기에는, main 함수가 끝난 후에도 계속 사용할 변수들을 저장할 수 있습니다.
+        변수를 사용하기 위해서는, 변수를 최초에 초기화해야 합니다.
+        (아래 예시)
+        self.prev_angle = 0
+        '''
     
     def main(self):
         '''
@@ -61,6 +69,10 @@ class Brain():
             (y : y 방향 상대 좌표 차이)
             (yaw : 배달 공간 방향 차이)
             (num : 배달 도착 공간의 번호)
+
+        # 위에서 선언한 클래스 변수에 값을 대입하는 방법
+        (아래 예시 : 현재 루프에서 사용한 각도를 클래스 내에 저장함.)
+        self.prev_angle = angle
 
         # 최종적으로 차량의 각도와 속도 결정
         return angle, speed
